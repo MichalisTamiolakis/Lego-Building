5005330d6c3579c239da8fd36ce65cc5e039689b
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2023-05-15 22:54:58 +0300

    Added comments in command

--- Lego Builder-VR/Assets/Scripts/Recorder/Command.cs
+++ Lego Builder-VR/Assets/Scripts/Recorder/Command.cs
@@ -12,17 +12,39 @@ public class Command
         MoveHand
     }
 
+    /// <summary>
+    /// The block this command refers to
+    /// </summary>
+    public int blockID;
+
+    /// <summary>
+    /// Used for Require Brick Command to describe the color of the brick
+    /// </summary>
     public LegoBrick.BrickColor brickColor;
+    /// <summary>
+    /// Used for Require Brick Command to describe the type of the brick
+    /// </summary>
     public LegoBrick.BrickType brickType;
    
-    public int blockID;
+    /// <summary>
+    /// The animation frames, used for MoveBrick and MoveHand commands
+    /// </summary>
     public List<AnimationFrame> frames;
 }
 
 [System.Serializable]
 public class AnimationFrame
 {
+    /// <summary>
+    /// The position of this frame
+    /// </summary>
     public Vector3 position = Vector3.zero;
+    /// <summary>
+    /// The rotation of this frame
+    /// </summary>
     public Quaternion rotation = Quaternion.identity;
+    /// <summary>
+    /// The frame timestamp in seconds
+    /// </summary>
     public float timestamp = 0f;
 }
