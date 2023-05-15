4bb5ccd950b6c37de8b691fc49adc94984d1d201
Alexandra Alexandridi <alexandrid@ics.forth.gr>
2023-05-15 21:07:07 +0300

    added menu scene

---
+++ Lego Builder-AR/Assets/scripts/Instatiate.cs
@@ -0,0 +1,65 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class Instatiate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public GameObject card;
+    public GameObject brick_num;
+    
+
+    [System.Serializable]
+    public class Model
+    {
+        public int id;
+        public string name;
+        public int time;
+        public string level;
+        public int bricks;
+    }
+
+    public void printModel(Model model)
+    {
+        Debug.Log("Name: " + model.name + ", Time: " + model.time + ", Level: " + model.level + ", Bricks: " + model.bricks + "\n");
+    }
+
+    void Start()
+    {
+        string path = Application.dataPath + "/json/Models.json";
+        
+        string jsonString = File.ReadAllText(path);
+        Model[] models = JsonConvert.DeserializeObject<Model[]>(jsonString);
+        for (int i = 0; i < models.Length; i++)
+        {
+            string duration = "";
+            int minutes = models[i].time / 60;
+            int seconds = models[i].time % 60;
+            if (minutes != 0) duration += minutes + " minutes";
+            if (seconds != 0) duration += " and " + seconds + " seconds";
+            GameObject instantiatedPrefab = Instantiate(card, transform);
+            GameObject time = instantiatedPrefab.transform.Find("information/content/time/time_text").gameObject;
+            GameObject bricks = instantiatedPrefab.transform.Find("information/content/bricks/bricks_num").gameObject;
+            GameObject level = instantiatedPrefab.transform.Find("information/content/level/level_text").gameObject;
+            GameObject name = instantiatedPrefab.transform.Find("header/name").gameObject;
+            GameObject state = instantiatedPrefab.transform.Find("header/state").gameObject;
+            time.GetComponent<Text>().text = duration;
+            bricks.GetComponent<Text>().text = models[i].bricks.ToString();
+            level.GetComponent<Text>().text = models[i].level;
+            name.GetComponent<Text>().text = models[i].name;
+            state.GetComponent<Text>().text = "0% Completed";
+
+            clickHandler onClickHandler = instantiatedPrefab.GetComponent<clickHandler>();
+            onClickHandler.prefabId = models[i].id;
+        }
+        //GameObject instantiatedPrefab = Instantiate(card, transform);
+        Debug.Log("Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Lego Builder-AR/Assets/scripts/clickHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class clickHandler : MonoBehaviour, IPointerClickHandler
+{
+
+    public int prefabId;
+    //public sceneNam
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Card " + prefabId + " clicked!");
+        SceneManager.LoadScene("BuildingScene", LoadSceneMode.Single);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.GetActiveScene().GetRootGameObjects()[0].GetComponent<LoadModel>().SetPrefabId(prefabId);
+    }
+}
