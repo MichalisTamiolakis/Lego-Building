fd0943de8a5e6118da934b34dd1666c236c8edb4
Alexandra Alexandridi <alexandrid@ics.forth.gr>
2023-06-01 22:00:26 +0300

    added settings

--- Lego Builder-AR/Assets/scripts/clickHandler.cs
+++ Lego Builder-AR/Assets/scripts/clickHandler.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class clickHandler : MonoBehaviour, IPointerClickHandler
+public class clickHandler : MonoBehaviour
+    //, IPointerClickHandler
 {
-
-    public int prefabId;
+    public GameObject canvas;
+    public int prefabId ;
     //public sceneNam
     // Start is called before the first frame update
     void Start()
@@ -21,16 +22,16 @@ public class clickHandler : MonoBehaviour, IPointerClickHandler
         
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    /*public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Card " + prefabId + " clicked!");
+        //Debug.Log("Card " + prefabId + " clicked!");
         SceneManager.LoadScene("BuildingScene", LoadSceneMode.Additive);
         SceneManager.sceneLoaded += OnSceneLoaded;
-    }
+    }*/
 
     public void ChangeScene()
     {
-        Debug.Log("Card " + prefabId + " clicked!");
+        
         SceneManager.UnloadSceneAsync("Menu");
         SceneManager.LoadScene("BuildingScene", LoadSceneMode.Additive);
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -39,6 +40,8 @@ public class clickHandler : MonoBehaviour, IPointerClickHandler
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
-        SceneManager.GetActiveScene().GetRootGameObjects()[0].GetComponent<LoadModel>().SetPrefabId(prefabId);
+        Debug.Log("Card " + prefabId + " clicked!");
+        //Debug.Log("Debug: " + SceneManager.GetActiveScene().GetRootGameObjects());
+        //SceneManager.GetActiveScene().GetRootGameObjects()[0].GetComponent<LoadModel>().SetPrefabId(prefabId);
     }
 }
